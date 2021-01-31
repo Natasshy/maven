322fa26fc5ac38f9ffdb4557b679addfa559e084
Natasshy <dolmatova-nata@mail.ru>
2021-01-31 22:30:18 +0300

    task1 task1_1

---
+++ src/main/java/TASK1.java
@@ -0,0 +1,36 @@
+
+/*
+1. Начинаем писать калькулятор:
+
+Реализовать сложение дробных чисел, которые пользователь вводит в консоль.
+Использовать комментарии и JavaDoc при описании метода.
+Использовать форматирование при выводе результата в консоль.
+Полученный результат округлять до 4-х знаков после запятой.
+ */
+
+/**
+ * @author Natalia Dolmatova
+ */
+
+import java.util.Scanner;
+
+public class TASK1 {
+    public static void main(String[] args) {
+        /**
+         * @param x - первое дробное число, вводимое пользователем
+         * @param x - второе дробное число, вводимое пользователем
+         * @param result - результат сложения двух дробных чисел
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое дробное число: ");
+        double x = sc.nextDouble();
+        System.out.println("Введите первое второе число: ");
+        double y = sc.nextDouble();
+        double result = x + y;
+
+        System.out.printf("Полученный результат: %.4f", result);
+
+        sc.close();
+    }
+
+}
---
+++ src/main/java/TASK1_1.java
@@ -0,0 +1,56 @@
+
+/*
+1. Начинаем писать калькулятор:
+
+Реализовать сложение дробных чисел, которые пользователь вводит в консоль.
+Использовать комментарии и JavaDoc при описании метода.
+Использовать форматирование при выводе результата в консоль.
+Полученный результат округлять до 4-х знаков после запятой.
+*Добавить возможность выбора другой операции (сложение,
+вычитание, деление, умножение)
+ */
+
+/**
+ * @author Natalia Dolmatova
+ */
+
+import java.util.Scanner;
+
+public class TASK1_1 {
+    public static void main(String[] args) {
+        /**
+         * @param x - первое дробное число, вводимое пользователем
+         * @param x - второе дробное число, вводимое пользователем
+         * @param result - результат сложения/вычетания/деления/сложения двух дробных чисел
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите знак необходимой арифметической операции: +, -, /, *: ");
+        String string = sc.next();
+
+        System.out.println("Введите первое дробное число: ");
+        double x = sc.nextDouble();
+        System.out.println("Введите первое второе число: ");
+        double y = sc.nextDouble();
+        double result = 0;
+
+        if (string == "+") {
+              result = x + y;
+        }
+       else if (string == "-") {
+               result = x - y;
+        }
+
+       else if (string == "/") {
+               result = x / y;
+        }
+
+       else if (string == "*") {
+               result = x * y;
+        }
+
+        System.out.printf("Полученный результат: %.4f", result);
+
+        sc.close();
+    }
+
+}
