818e72c0abe3f29d83a0e2774e95424abb1adc3c
Natasshy <dolmatova-nata@mail.ru>
2021-01-31 22:34:45 +0300

    исправила на второе задание

--- src/main/java/TASK2.java
+++ src/main/java/TASK2.java
@@ -6,8 +6,6 @@
 Использовать комментарии и JavaDoc при описании метода.
 Использовать форматирование при выводе результата в консоль.
 Полученный результат округлять до 4-х знаков после запятой.
-*Добавить возможность выбора другой операции (сложение,
-вычитание, деление, умножение)
  */
 
 /**
@@ -16,37 +14,19 @@
 
 import java.util.Scanner;
 
-public class TASK1_1 {
+public class TASK2 {
     public static void main(String[] args) {
         /**
          * @param x - первое дробное число, вводимое пользователем
          * @param x - второе дробное число, вводимое пользователем
-         * @param result - результат сложения/вычетания/деления/сложения двух дробных чисел
+         * @param result - результат сложения двух дробных чисел
          */
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите знак необходимой арифметической операции: +, -, /, *: ");
-        String string = sc.next();
-
         System.out.println("Введите первое дробное число: ");
         double x = sc.nextDouble();
         System.out.println("Введите первое второе число: ");
         double y = sc.nextDouble();
-        double result = 0;
-
-        if (string == "+") {
-              result = x + y;
-        }
-       else if (string == "-") {
-               result = x - y;
-        }
-
-       else if (string == "/") {
-               result = x / y;
-        }
-
-       else if (string == "*") {
-               result = x * y;
-        }
+        double result = x + y;
 
         System.out.printf("Полученный результат: %.4f", result);
 
