e66d428fc61a1371962380a7cb7aa1c3823fdc62
Natasshy <dolmatova-nata@mail.ru>
2021-02-02 23:51:06 +0300

    3 задание

---
+++ src/main/java/Task3_1.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Natalia Dolmatova
+ */
+
+/*import java.util.Scanner;
+
+public class Task3_1 {
+
+
+    public static void main (String [] args){
+        System.out.println("Выберете программу: 1 - калькулятор, 2 - поиск максимального слова в массиве");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        if (a == "1") {
+
+        }
+}
+
+ */
