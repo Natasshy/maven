54f95f8ef3e7f88bdea503329818a2ae228544a8
Natasshy <dolmatova-nata@mail.ru>
2021-03-02 22:33:00 +0300

    изменение в джавадоке

--- src/main/java/Task2_22.java
+++ src/main/java/Task2_22.java
@@ -10,35 +10,30 @@
 вычитание, деление, умножение)
  */
 
-/**
- * @author Natalia Dolmatova
- */
+
 
 import java.util.Scanner;
 
 public class Task2_22 {
-    public static void main(String[] args) {
+
+    /**
+     * @author Natalia Dolmatova
+     */
+        public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите знак необходимой арифметической операции: +, -, /, *: ");
 
-        /**
-         * * @param znak - знак необходимой арифметической операции
-         */
+
         char znak = sc.next().charAt(0);
 
-        /**
-         * @param x - первое дробное число, вводимое пользователем
-         * @param y - второе дробное число, вводимое пользователем
-         */
+
         System.out.println("Введите первое дробное число: ");
         double x = sc.nextDouble();
         System.out.println("Введите второе дробное число: ");
         double y = sc.nextDouble();
 
-        /**
-         *   @param result - результат сложения/вычетания/деления/умножения двух дробных чисел
-         */
+
         double result = 0;
 
 
