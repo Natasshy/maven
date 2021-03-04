910c42e08c49f38ad565e825c23c304a138ff84d
Natasshy <dolmatova-nata@mail.ru>
2021-03-04 23:13:38 +0300

    Формируется новогодний подарок.

--- src/main/java/Task4_2.java
+++ src/main/java/Task4_2.java
@@ -6,78 +6,82 @@
  Найти общий вес подарка, общую стоимость подарка и вывести на консоль информацию о всех сладостях в подарке.
  */
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Task4_2 {
     public static void main(String[] args) {
-        String nameCandy = "Candy";
-        int weightCandy =  75;
-        int priceCandy = 10;
-        int idCandy = 1;
+        Scanner scanner = new Scanner(System.in);
+        Items Candy = new Items("Candy", 10, 1);
+        Items Jellybean = new Items("Jellybean", 15, 5);
+        Items Chocolate = new Items("Chocolate", 20, 3);
 
-        String nameJellybean = "Jellybean";
-        int weightJellybean =  55;
-        int priceJellybean = 35;
-        int idJellybean = 2;
-
-        String nameChocolate = "Chocolate";
-        int weightChocolate =  90;
-        int priceChocolate =  15;
-        int idChocolate = 3;
-
-        String nameCookies = "Cookies";
-        int weightCookies =  90;
-        int priceCookies =  25;
-        int idCookies = 4;
+        List<Items> productList = new ArrayList<>();
+        productList.add(Candy);
+        productList.add(Jellybean);
+        productList.add(Chocolate);
 
 
-        Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("Введите id для добавление в подарок - Candy 1, Jellybean 2, Chocolate 3, Cookies 4 ");
-
-
-        int choice = sc.nextInt();
-
-
-
-         switch(choice) {
-            case 1:
-                System.out.println("Вы выбрали: " + nameCandy);
-                System.out.println("Общий вес: " + weightCandy);
-                System.out.println("Общая стоимость: " + priceCandy);
-                System.out.println("Уникальный параметр: " + idCandy);
-
-                break;
-
-            case 2:
-                System.out.println("Вы выбрали: " + nameJellybean);
-                System.out.println("Общий вес: " + weightJellybean);
-                System.out.println("Общая стоимость: " + priceJellybean);
-                System.out.println("Уникальный параметр: " + idJellybean);
-
-                break;
-
-            case 3:
-                System.out.println("Вы выбрали: " + nameChocolate);
-                System.out.println("Общий вес: " + weightChocolate);
-                System.out.println("Общая стоимость: " + priceChocolate);
-                System.out.println("Уникальный параметр: " + idChocolate);
-
-                break;
-
-
-            case 4:
-                System.out.println("Вы выбрали: " + nameCookies);
-                System.out.println("Общий вес: " + weightCookies);
-                System.out.println("Общая стоимость: " + priceCookies);
-                System.out.println("Уникальный параметр: " + idCookies);
-
-                break;
-
+        System.out.println("Выберете сладость для добавления в новогодний подарок, по окончании введите end: ");
+        for (int i = 1; i < productList.size() + 1; i++) {
+            System.out.println(i + ". " + productList.get(i - 1).getName() + " - " + productList.get(i - 1).getPrice() + ";");
         }
+        while (true) {
+            System.out.println("Введите номер сладости: ");
+            String input = scanner.nextLine();
+            if (input.equals("End") || input.equals("end")) {
+                break;
+            }
+            int index = Integer.parseInt(input);
+
+            System.out.println("Введите количество сладостей: ");
+            String inputQuantity = scanner.nextLine();
+            try {
+                int quantity = Integer.parseInt(inputQuantity);
+                productList.get(index - 1).getResult(quantity);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Сладости с таким кодом не существует");
+            }
+        }
+
+        System.out.println("Общее количетво сладостей в подарке: " + Items.totalQuantity);
+        System.out.println("Общая цена: " + Items.totalPrice);
+        System.out.println("Общий вес: " + Items.totalWeight);
+        System.out.println("Подарок состоит из: " + Items.getName);
 
 
     }
+
 }
+
+class Items {
+    public static String getName;
+    private String name;
+    private int price;
+    private int weight;
+    protected static int totalPrice = 0;
+    protected static int totalQuantity = 0;
+    protected static int totalWeight = 0;
+
+    Items(String name, int price, int weight) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public void getResult(int quantity) {
+        this.totalQuantity += quantity;
+        this.totalPrice += (price * quantity);
+        this.totalWeight += weight;
+        this.getName += name + " ";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
+
