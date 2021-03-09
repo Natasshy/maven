7dd4a8d2cf99d626a6fba76fdc3ee82572a66ad2
Natasshy <dolmatova-nata@mail.ru>
2021-03-09 20:50:48 +0300

    09/03  Напиши программку которая спрашивает в начале имя, затем фамилию, затем отчество.   Потом спрашивает год рождения.   И после этого выводит итоговую информацию в консоль в виде ФИО и возраста  Возраст вычисляй просто = 2021 - полученный год

--- src/main/java/Beginner.java
+++ src/main/java/Beginner.java
@@ -1,31 +1,40 @@
-
-/*Написать три метода - один метод соедединяет две строки и возвращает результат,
-второй метод - выводит в консоль результат соединения двух строк
-третий метод- печатает привет
+/*Напиши программку которая спрашивает в начале имя, затем фамилию, затем отчество.
+ Потом спрашивает год рождения.
+ И после этого выводит итоговую информацию в консоль в виде ФИО и возраста
+Возраст вычисляй просто = 2021 - полученный год
  */
 
+import java.util.Scanner;
 
 public class Beginner {
     public static void main(String[] args) {
-        concatString("q", "a");
-        printConcat("we", " are the best");
-        printHello();
+        System.out.println("ФИО: " + ascName() + " " + ascSecondName() + " " + ascPatronymic() + ", возраст: " + ascAge() + " лет");
 
     }
 
-    static String concatString(String a, String b) {
-        return a + b;
+    public static String ascName() {
+        System.out.println("Введите имя: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 
-    static String printConcat(String a, String b) {
-        System.out.println(a + b);
-        return a + b;
+    public static String ascSecondName() {
+        System.out.println("Введите фамилию: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 
-
-    static void printHello() {
-        System.out.println("Hello");
-
+    public static String ascPatronymic() {
+        System.out.println("Введите отчество: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
+
+    public static int ascAge() {
+        System.out.println("Введите год рождения: ");
+        Scanner sc = new Scanner(System.in);
+        return 2021 - sc.nextInt();
+    }
+
 }
 
