8a01b8522122ff61c698cb3422943c750e341e8d
Natasshy <dolmatova-nata@mail.ru>
2021-03-12 14:17:56 +0300

    12 марта

--- src/main/java/Task4_2.java
+++ src/main/java/Task4_2.java
@@ -6,6 +6,7 @@
 // Найти общий вес подарка, общую стоимость подарка и вывести на консоль информацию о всех сладостях в подарке.
 //
 
+import javax.swing.text.html.HTMLDocument;
 import java.util.*;
 
 public class Task4_2 {
@@ -13,19 +14,9 @@ public class Task4_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Corzina corzina = new Corzina();
-        Items Candy = new Items("Candy", 10, 1);
-        Items Jellybean = new Items("Jellybean", 15, 5);
-        Items Chocolate = new Items("Chocolate", 20, 3);
-
-        corzina.addToCorzina(Candy);
-        corzina.addToCorzina(Jellybean);
-        corzina.addToCorzina(Chocolate);
-
-
         System.out.println("Выберете сладость для добавления в новогодний подарок, по окончании введите end: ");
-        for (int i = 1; i < ArrayList.size() + 1; i++) {
-            System.out.println(i + ". " + ArrayList.get(i - 1).getName() + " - " + ArrayList.get(i - 1).getPrice() + ";");
-        }
+        System.out.println(corzina.printOption());
+
         while (true) {
             System.out.println("Введите номер сладости: ");
             String input = scanner.nextLine();
@@ -38,76 +29,113 @@ public class Task4_2 {
             String inputQuantity = scanner.nextLine();
             try {
                 int quantity = Integer.parseInt(inputQuantity);
-                ArrayList.get(index - 1).getResult(quantity);
+                switch (index) {
+                    case 1 -> corzina.addCandy(quantity);
+                    case 2 -> corzina.addJellybean(quantity);
+                    case 3 -> corzina.addChocolate(quantity);
+                }
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Сладости с таким кодом не существует");
             }
         }
-
-        System.out.println("Общее количетво сладостей в подарке: " + Corzina.getTotalQuantity());
-        System.out.println("Общая цена: " + Corzina.getTotalPrice());
-        System.out.println("Общий вес: " + Corzina.getTotalWeight());
-        System.out.println("Подарок состоит из: " + Corzina.printName());
-    }
-
+        System.out.println("Общее количетво сладостей в подарке: " + corzina.getTotalQuantity());
+        System.out.println("Общая цена: " + corzina.getTotalPrice());
+        System.out.println("Общий вес: " + corzina.getTotalWeight());
+        System.out.println("Подарок состоит из: " + corzina.getDescription());
 
     }
 
-     class Items {
 
-        private String name;
-        public int price;
-        public int weight;
+}
+
+class Item {
+
+    private   String name;
+    private   int price;
+    private   int weight;
 
 
-        Items(String name, int price, int weight) {
-            this.name = name;
-            this.price = price;
-            this.weight = weight;
+    Item(String name, int price, int weight) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
+
+
+class Corzina {
+   private ArrayList Candy = new ArrayList();
+    private ArrayList Jellybean = new ArrayList();
+    private  ArrayList Chocolate = new ArrayList();
+
+    private int totalQuantity ;
+    private int totalWeight ;
+    private int totalPrice ;
+
+
+    public void addCandy(int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Candy.add(new Item("Candy", 10, 1));
+            totalQuantity++;
+            totalWeight += 10;
+            totalPrice += 1;
         }
+    }
 
-        public String getName() {
-            return name;
+    public void addJellybean(int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Jellybean.add(new Item("Jellybean", 15, 5));
+            totalQuantity++;
+            totalWeight += 15;
+            totalPrice += 5;
         }
+    }
 
-        public int getPrice() {
-            return price;
+    public void addChocolate(int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Chocolate.add(new Item("Chocolate", 20, 3));
+            totalQuantity++;
+            totalWeight += 20;
+            totalPrice += 3;
         }
     }
 
 
-     class Corzina {
-        ArrayList<Items> arr = new ArrayList();
+    public int getTotalQuantity( ) {
+        return totalQuantity;
 
-
-
-        public void addToCorzina(Items items) {
-            arr.add(items);
-
-        }
-
-        public int getTotalQuantity(int quantity) {
-            int totalQuantity = 0;
-            return totalQuantity += quantity;
-        }
-
-
-        public int getTotalPrice(int price) {
-            int totalPrice = 0;
-            return totalPrice += price;
-        }
-
-        public int getTotalWeight(int weight) {
-            int totalWeight = 0;
-            return totalWeight += weight;
-        }
-
-        public void printName() {
-            for (Items e : arr
-            ) {
-                System.out.println(e.getName() + " " + e.price);
-            }
-        }
     }
+
+    public int getTotalWeight() {
+        return totalWeight;
+
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+
+    }
+
+    public String getDescription() {
+        return "Конфет - " + Candy.size() + ", Мармелад - " + Jellybean.size() + ", Шоколад - " + Chocolate.size();
+    }
+
+    public String printOption() {
+        return  "1 - Candy, 2 - Jellybean, 3 - Chocolate";
+    }
+}
+
 
 
