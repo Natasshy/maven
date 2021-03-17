aad3a46a242d5445f56160a1b25aa20f750b2301
Natasshy <dolmatova-nata@mail.ru>
2021-03-17 13:33:57 +0300

    Калькулятор в стиле ООП

---
+++ src/main/java/Task5/Calc.java
@@ -0,0 +1,26 @@
+package Task5;
+
+
+
+public class Calc {
+
+
+    public static void Plus(double a, double b) {
+        System.out.println("Полученный результат: " + (a + b));
+    }
+
+    public static void Minus(double a, double b) {
+        System.out.println("Полученный результат: " + (a - b));
+    }
+
+
+    public static void Divide(double a, double b) {
+        System.out.println("Полученный результат: " + (a * b));
+    }
+
+    public static void Multiply(double a, double b) {
+        System.out.println("Полученный результат: " + (a / b));
+    }
+
+
+}
---
+++ src/main/java/Task5/Main.java
@@ -0,0 +1,27 @@
+package Task5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calc calc = new Calc();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        double a = scanner.nextDouble();
+        System.out.println("Введите второе число");
+        double b = scanner.nextDouble();
+        System.out.println("Введите знак необходимой арифметической операции: +, -, /, *: ");
+        char znak = scanner.next().charAt(0);
+
+
+        switch (znak) {
+            case '+' -> calc.Plus(a, b);
+            case '-' -> calc.Minus(a, b);
+            case '*' -> calc.Divide(a, b);
+            case '/' -> calc.Multiply(a, b);
+        }
+
+    }
+
+}
