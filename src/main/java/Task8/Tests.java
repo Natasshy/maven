a7a187bce4f6ff2fcc2945fa82248f2d1209455e
Natasshy <dolmatova-nata@mail.ru>
2021-04-18 13:59:45 +0300

    Покрыть Unit тестами последний проект с калькулятором

--- src/main/java/Task8/Tests.java
+++ src/main/java/Task8/Tests.java
@@ -1,56 +1,29 @@
 package Task8;
 
-import org.junit.*;
+;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class Tests {
 
-    private Calcul calculator;
-
-    @BeforeClass
-    public static void beforeClass() {
-        System.out.println("Before CalculatorTest.class");
-    }
-
-    @AfterClass
-    public  static void afterClass() {
-        System.out.println("After CalculatorTest.class");
-    }
-
-    @Before
-    public void unitTest() {
-        calculator = new Calcul();
-    }
-
-    @After
-    public void afterTest() {
-        calculator = null;
-    }
-
     @Test
     public void testGetSum() throws Exception {
-        assertEquals(15.0, calculator.Plus(7,8));
+        Calcul calculator = new Calcul();
+        assertEquals(15.0, calculator.Plus(7, 8));
     }
 
     @Test
     public void testGetDivide() throws Exception {
-        assertEquals(20.0, calculator.Divide(100,5));
+        Calcul calculator = new Calcul();
+        assertEquals(20.0, calculator.Divide(100, 5));
     }
 
-    @Test
-    public void testGetMultiple() throws Exception {
-
-    }
 
     @Test
     public void divisionWithException() {
-        calculator.Divide(15,0);
+        Calcul calculator = new Calcul();
+        calculator.Divide(15, 0);
     }
-
-
-
 }
 
 
